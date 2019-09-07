76e3705337fb3fb05c3f68266367ee2dae7fe5cd
rechitpatni <rechitpatni@gmail.com>
2019-09-07 14:30:47 +0530

    Second change

--- Workshop1.cpp
+++ Workshop1.cpp
@@ -6,12 +6,11 @@ using namespace std;
 int main()
 {
     int a=10;
-
     cout<<"Learning git and github ";
     cout<<"Basics of git and github";
     cout<<a;
-
     //This is our first version..
+    //This is our second version..
     return 0;
 
 }
